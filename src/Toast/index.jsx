77242514ac03e3d6a2d5876812d06801df3f31e1
Sue Ann Ioanis <sue.ioanis@leaflink.com>
2020-01-02 05:00:36 -0500

    Remove draggable

--- src/Toast/index.jsx
+++ src/Toast/index.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useCallback } from "react";
-// import Draggable from "react-draggable";
 
 import "./style.css";
 
@@ -20,7 +19,6 @@ export default function Toast(props) {
   } = props;
 
   const [isOpenState, setOpen] = useState(false);
-  // const [isReset, setDraggable] = useState(false);
   const onClose = useCallback(() => {
     const toastElement = document.querySelectorAll(".ReactToast");
     if (closeCallback) closeCallback();
@@ -28,20 +26,14 @@ export default function Toast(props) {
     setTimeout(() => {
       if (toastElement) {
         toastElement[0].style = "";
-        setDraggable('close');
       }
     }, duration);
-  }, [closeCallback, setOpen, duration, setDraggable]);
-
-  const onDragStop = (e, d) => {
-    if (d && (d.x > 10 || d.x < -10)) onClose();
-  };
+  }, [closeCallback, setOpen, duration]);
 
   useEffect(() => {
     if (timeout) clearTimeout(timeout);
     if (openTimeout) clearTimeout(openTimeout);
     if (isOpen && (isOpen !== isOpenState)) {
-      setDraggable('open');
       openTimeout = setTimeout(() => setOpen(true), 50);
     } else if (!isOpen) {
       setOpen(false);
@@ -52,24 +44,23 @@ export default function Toast(props) {
   }, [isOpen, duration, setOpen, autoDismiss, onClose, isOpenState]);
 
   return (
-    <Draggable axis="x" onStop={onDragStop} key={isReset}></Draggable>
-      <div
-        className={`ReactToast${
-          isOpenState ? ' isOpen' : ''}${
-            classNames.length ? classNames.join(' ').toString() : ''
-          }`
-        }
-      >
-        {title && <h2 className="ReactToast--title">{title}</h2>}
-        {description && (
-          <div className="ReactToast--description">{description}</div>
-        )}
-        {children}
-        {hasCloseBtn && (
-          <button className="ReactToast--close" onClick={onClose}>
-            &times;
-          </button>
-        )}
-      </div>
+    <div
+      className={`ReactToast${
+        isOpenState ? ' isOpen' : ''}${
+          classNames.length ? classNames.join(' ').toString() : ''
+        }`
+      }
+    >
+      {title && <h2 className="ReactToast--title">{title}</h2>}
+      {description && (
+        <div className="ReactToast--description">{description}</div>
+      )}
+      {children}
+      {hasCloseBtn && (
+        <button className="ReactToast--close" onClick={onClose}>
+          &times;
+        </button>
+      )}
+    </div>
   );
 }
