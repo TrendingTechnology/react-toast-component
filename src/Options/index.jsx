44d02fd2281a0bae1c2a1b070f067d15ee216b76
sue <sueann.ioanis@gmail.com>
2020-01-04 19:51:29 -0500

    Lazy loading

--- src/Options/index.jsx
+++ src/Options/index.jsx
@@ -83,7 +83,7 @@ function Options(props) {
           );
         })}
       </div>
-      <img className="Options--img" src="toast.png" alt="" />
+      <img className="Options--img" src="toast.png" loading="lazy" alt="..." />
       <p className="Options--attr">credit: toaster &amp; friends
           by <a href="https://dribbble.com/shots/7667697-toaster-friends">KOIOS design</a></p>
       <p className="Options--icons">
