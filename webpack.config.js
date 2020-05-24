7fdbf0078546f16a7fa2051bfb51f560af34a8f4
sue <sueann.ioanis@gmail.com>
2020-05-23 22:42:52 -0400

    Publishing

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,78 @@
 var path = require("path");
 
-module.exports = {
-  mode: "production",
-  entry: "./src/Toast/index.jsx",
-  output: {
-    path: path.resolve(__dirname, "public/lib"),
-    filename: "Toast.js",
-    library: "Toast",
-    libraryExport: "default",
-    libraryTarget: "umd",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        use: "babel-loader",
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-  externals: {
-    react: {
-      root: "React",
-      commonjs: "react",
-      commonjs2: "react",
-      amd: "react",
+module.exports = [
+  {
+    mode: "production",
+    entry: "./src/Toast/index.jsx",
+    output: {
+      path: path.resolve(__dirname, "public/lib"),
+      filename: "Toast.js",
+      library: "Toast",
+      libraryExport: "default",
+      libraryTarget: "umd",
     },
-    "react-dom": {
-      root: "ReactDOM",
-      commonjs2: "react-dom",
-      commonjs: "react-dom",
-      amd: "react-dom",
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          use: "babel-loader",
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
+    externals: {
+      react: {
+        root: "React",
+        commonjs: "react",
+        commonjs2: "react",
+        amd: "react",
+      },
+      "react-dom": {
+        root: "ReactDOM",
+        commonjs2: "react-dom",
+        commonjs: "react-dom",
+        amd: "react-dom",
+      },
     },
   },
-};
+  {
+    mode: "development",
+    entry: "./src/Toast/index.jsx",
+    output: {
+      path: path.resolve(__dirname, "lib"),
+      filename: "Toast.js",
+      library: "Toast",
+      libraryExport: "default",
+      libraryTarget: "umd",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          use: "babel-loader",
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
+    externals: {
+      react: {
+        root: "React",
+        commonjs: "react",
+        commonjs2: "react",
+        amd: "react",
+      },
+      "react-dom": {
+        root: "ReactDOM",
+        commonjs2: "react-dom",
+        commonjs: "react-dom",
+        amd: "react-dom",
+      },
+    },
+  },
+];
